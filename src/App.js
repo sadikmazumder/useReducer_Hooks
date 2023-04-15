55fcb8a5372aa4fe1ebbd3ca5f14b16fa4005b53
HM Sadik Ahsan Mazumder <38665358+sadikmazumder@users.noreply.github.com>
2023-04-15 16:07:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Counter from "./cmponent/Counter";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>useReducer</h1>
+      <Counter />
+    </div>
+  );
+}
